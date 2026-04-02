646693bce45150a3f9dd780c8cf04b87c0feac56
bagusabdulkarim <bagusabdulkarim@gmail.com>
2026-04-02 15:04:12 +0700

    Kelola Pelanggan dan Produk fix

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,13 @@ use App\Http\Controllers\Api\ProdukController;
 use App\Http\Controllers\Api\PelangganController;
 
 Route::get('/produk',[ProdukController::class,'index']);
+Route::get('/produk/{id}',[ProdukController::class,'show']);
+Route::post('/Produk',[ProdukController::class,'store']);
+Route::put('/Produk/{id}',[ProdukController::class,'update']);
+Route::delete('/Produk/{id}',[ProdukController::class,'destroy']);
+
+Route::get('/pelanggan',[PelangganController::class,'index']);
+Route::get('/pelanggan/{id}',[PelangganController::class,'show']);
+Route::post('/pelanggan',[PelangganController::class,'store']);
+Route::put('/pelanggan/{id}',[PelangganController::class,'update']);
+Route::delete('/pelanggan/{id}',[PelangganController::class,'destroy']);
