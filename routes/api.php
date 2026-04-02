828cb199d95427ef2aedae1efaf60332233abebb
Ariffurqan11 <ariffurqan10@guru.smk.belajar.id>
2026-04-02 13:23:07 +0700

    Kelola data Pelanggan

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProdukController;
+use App\Http\Controllers\Api\PelangganController;
 
 Route::get('/produk',[ProdukController::class,'index']);
+
+
+Route::get('/pelanggan',[PelangganController::class,'index']);
+Route::get('/pelanggan/{id}',[PelangganController::class,'show']);
+Route::post('/pelanggan',[PelangganController::class,'store']);
+Route::put('/pelanggan/{id}',[PelangganController::class,'update']);
+Route::delete('/pelanggan/{id}',[PelangganController::class,'destroy']);
